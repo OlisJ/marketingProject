ce9e835d256d5f3ead0e28c1f5edda34e086d5d9
OlisJ <olisj794@gmail.com>
2025-04-17 17:37:24 +0200

    .

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php  get_header();?>
 <nav class="navbar navbar-expand-lg " style="background-color:	#38658b;" data-bs-theme="dark">
   <div class="container-fluid">
-    <a class="navbar-brand" href="#">Navbar</a>
+    <a class="navbar-brand" href="#">ChainPulse</a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
@@ -24,18 +24,45 @@
   </div>
 </nav>
 
-<h1 id="h1">Web 3</h1>
+<h1 id='compname'>ChainPulse</h1>
+<br><br>
 
-
-<div class="card" style="width: 18rem;">
-  <img src="<?php echo get_template_directory_uri();?> /img/bc.png" class="card-img-top" >
-  <div class="card-body">
-    <h5 class="card-title">Card title</h5>
-    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-    <a href="#" class="btn btn-primary">Go somewhere</a>
+<div class='cardgrid'>
+   <div class="card" style="width: 21rem;">
+    <img src="<?php echo get_template_directory_uri();?> /img/bitcoin.jpg" class="card-img-top" >
+    <div class="card-body">
+      <h5 class="card-title">Whale Strategy Revealed As BTC Spikes 37%</h5>
+      <p class="card-text">While the crypto market remains shaken by speculative tremors, a subtle signal captures the insiders’ attention.</p>
+      <a href="#" class="btn btn-primary">Go somewhere</a>
+    </div>
   </div>
-</div>
 
+  <div class="card" style="width: 21rem;">
+    <img src="<?php echo get_template_directory_uri();?> /img/pi.png" class="card-img-top" >
+    <div class="card-body">
+      <h5 class="card-title">Pi Network’s Coin Risks Crashing Another 25%</h5>
+      <p class="card-text">Pi Coin (PI), may implode further after crashing 80% from its yearly high.</p>
+      <a href="#" class="btn btn-primary">Go somewhere</a>
+    </div>
+  </div>
+
+  <div class="card" style="width: 21rem;">
+    <img src="<?php echo get_template_directory_uri();?> /img/bitcoin.jpg" class="card-img-top" >
+    <div class="card-body">
+      <h5 class="card-title">Ethereum fees drop to a 5-year low </h5>
+      <p class="card-text">Transaction costs on the Ethereum network have dropped to the lowest level in five years</p>
+      <a href="#" class="btn btn-primary">Go somewhere</a>
+    </div>
+  </div>
+
+  <div class="card" style="width: 21rem;">
+    <img src="<?php echo get_template_directory_uri();?> /img/eth1.jpg" class="card-img-top" >
+    <div class="card-body">
+      <h5 class="card-title">Bitcoin Trades Around $84,000 as Crypto Stocks Slip</h5>
+      <p class="card-text">The leading currency recently traded around $84,000, rising back above the prices below $77,000 seen late last week</p>
+      <a href="#" class="btn btn-primary">Go somewhere</a>
+    </div>
+  </div>
 
 <div >
 
